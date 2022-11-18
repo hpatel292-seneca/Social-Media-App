1b22f40a3a7c2fec06e4e78fd0493845a5000683
Harshil Patel <100322816+hpatel292-seneca@users.noreply.github.com>
2022-11-18 08:22:37 +0000

    Commiting changes

--- client/src/Components/Posts/Post.js
+++ client/src/Components/Posts/Post.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Post from './Post/post'
 import useStyles from "./styles"
+import { useSelector } from 'react-redux';
 
 const Posts = () =>{
+    const state = useSelector((state)=> state.posts);
+    console.log(state);
  const classes = useStyles();
     return (
         <>
--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,12 @@
 import * as api from '../api';
 
 // Action creators
-const getPost = () => {
-    const action = {type: 'FATCH_ALL', payload: []}
-
-    return action;
+const getPost = () => async(dispatch)=>{
+    try {
+        const {data} = api.fetchPosts;
+        const action = {type: 'FATCH_ALL', payload: data};
+        dispatch(action);
+    } catch (error) {
+        console.log(error);
+    }
 }
